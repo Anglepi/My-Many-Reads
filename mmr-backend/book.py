57818c0a219ab7f0cb6a4c411e6166e58e419519
Ángel Píñar Rivas <angle@correo.ugr.es>
2022-09-07 18:41:13 +0200

    #31 ADD basic book data structure

---
+++ mmr-backend/book.py
@@ -0,0 +1,13 @@
+from __future__ import annotations
+from enum import Enum
+
+
+class Book:
+    def __init__(self, id: str, title: str, synopsis: str, author: str, genres: list[Book.Genre], publisher: str, publishing_date: str):
+        self.__id: str = id
+        self.__title: str = title
+        self.__synopsis: str = synopsis
+        self.__author: str = author
+        self.__genres: list[Book.Genre] = genres
+        self.__publisher: str = publisher
+        self.__publishing_date: str = publishing_date
