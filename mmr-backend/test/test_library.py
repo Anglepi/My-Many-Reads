814a6f1e128477fc82e829ffe0ade13c6f89cab8
Ángel Píñar Rivas <angle@correo.ugr.es>
2022-09-19 13:09:41 +0200

    #35 ADD couple extra test cases

--- mmr-backend/test/test_library.py
+++ mmr-backend/test/test_library.py
@@ -32,6 +32,35 @@ def test_create_library_with_entries():
     check_library_data(library, owner, name, entries)
 
 
+def test_add_entry():
+    # Given
+    owner = "Sergio"
+    name = "Libros de historia"
+    library = Library(owner, name)
+    entry = get_sample_entries()[0]
+
+    # When
+    library.add_entry(entry)
+
+    # Then
+    check_library_data(library, owner, name, [entry])
+
+
+def test_remove_entry():
+    # Given
+    owner = "Sergio"
+    name = "Libros de historia"
+    library = Library(owner, name, [Library.Entry(
+        "first book id", 7, "PLAN TO READ"), Library.Entry("second book id", 10, "COMPLETED")].copy())
+    expected_entries = [get_sample_entries()[0]]
+
+    # When
+    library.remove_entry("second book id")
+
+    # Then
+    check_library_data(library, owner, name, expected_entries)
+
+
 def check_library_data(library: Library, expected_owner: str, expected_name: str, expected_entries: list[Library.Entry]):
     assert_that(library.get_name()).is_equal_to(expected_name)
     assert_that(library.get_owner()).is_equal_to(expected_owner)
