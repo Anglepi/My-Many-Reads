bc5a684bea967eec9bc8f1ce63ffc003ac864024
Ángel Píñar Rivas <angle@correo.ugr.es>
2022-10-12 19:32:55 +0200

    #47 ADD test for endpoints

--- mmr-backend/test/test_main.py
+++ mmr-backend/test/test_main.py
@@ -1,8 +1,19 @@
 from assertpy import assert_that
 from fastapi.testclient import TestClient
 from main import mmr
+import os
+import json
+from book import Book
 
 client = TestClient(mmr)
+
+
+current_dir = os.path.dirname(__file__)
+sample_data_path = "sample-data/books.json"
+data_path = os.path.join(current_dir, sample_data_path)
+with open(data_path) as json_books:
+    books_data = json.load(json_books)
+    book_list: list[Book] = Book.from_list(books_data)
 
 
 def check_response(response, status, body):
@@ -14,5 +25,29 @@ def test_api_initialized():
     response = client.get("/")
     expected_status = 200
     expected_body = {"message": "Hello World!"}
+
+    check_response(response, expected_status, expected_body)
+
+
+def test_get_books():
+    response = client.get("/books")
+    expected_status = 200
+    expected_body = list(map(lambda book: book.to_dict(), book_list))
+
+    check_response(response, expected_status, expected_body)
+
+
+def test_get_book():
+    response = client.get("/books/ABookId")
+    expected_status = 200
+    expected_body = [book_list[0].to_dict()]
+
+    check_response(response, expected_status, expected_body)
+
+
+def test_get_book_not_found():
+    response = client.get("/books/IJustMadeThisUp")
+    expected_status = 200
+    expected_body = []
 
     check_response(response, expected_status, expected_body)
