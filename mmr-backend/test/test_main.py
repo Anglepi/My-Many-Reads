787cc790523163ed2f622073609cdbc40e349613
Ángel Píñar Rivas <angle@correo.ugr.es>
2022-10-15 17:21:31 +0200

    #48 ADD some api tests to manage libraries

--- mmr-backend/test/test_main.py
+++ mmr-backend/test/test_main.py
@@ -54,3 +54,56 @@ def test_get_book_not_found():
     expected_body = []
 
     check_response(response, expected_status, expected_body)
+
+
+def test_get_libraries():
+    with TestClient(mmr) as client:
+        response = client.get("/libraries/user1")
+    expected_status = 200
+    expected_body = [{"owner": "user1", "name": "myLibrary", "entries": []}, {
+        "owner": "user1", "name": "myOtherLibrary", "entries": []}]
+
+    check_response(response, expected_status, expected_body)
+
+
+def test_get_library():
+    with TestClient(mmr) as client:
+        response = client.get("/libraries/user2/generic")
+    expected_status = 200
+    expected_body = {"owner": "user2", "name": "generic", "entries": [
+        {"book_id": "RandomBook", "score": 5, "status": "COMPLETED"}]}
+
+    check_response(response, expected_status, expected_body)
+
+
+def test_create_library():
+    with TestClient(mmr) as client:
+        libraries = client.get("/libraries/userTest").json()
+        response = client.post("/libraries/userTest/libraryTest")
+        new_libraries = client.get("/libraries/userTest").json()
+
+    assert_that(response.status_code).is_equal_to(200)
+    assert_that(libraries).is_not_equal_to(new_libraries)
+    assert_that(new_libraries).is_equal_to(
+        [{"owner": "userTest", "name": "libraryTest", "entries": []}])
+
+
+def test_delete_nonexistent_library():
+    with TestClient(mmr) as client:
+        libraries = client.get("/libraries/userTest").json()
+        response = client.delete("/libraries/userTest/nonexistent")
+        new_libraries = client.get("/libraries/userTest").json()
+
+    assert_that(response.status_code).is_equal_to(200)
+    assert_that(libraries).is_equal_to(new_libraries)
+
+
+def test_delete_library():
+    with TestClient(mmr) as client:
+        libraries = client.get("/libraries/userTest").json()
+        response = client.delete("/libraries/userTest/libraryTest")
+        new_libraries = client.get("/libraries/userTest").json()
+
+    assert_that(response.status_code).is_equal_to(200)
+    assert_that(libraries).is_not_equal_to(new_libraries)
+    assert_that(new_libraries).is_equal_to([])
