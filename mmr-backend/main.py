25beb22708ba81998d86537b2177e53e6cb5dc7a
Ángel Píñar Rivas <angle@correo.ugr.es>
2022-10-15 14:16:37 +0200

    #48 ADD endpoints to manage libraries

--- mmr-backend/main.py
+++ mmr-backend/main.py
@@ -1,4 +1,6 @@
-from book import Book        # Given
+from ctypes.util import find_library
+from book import Book
+from library import Library
 import os
 import json
 from fastapi import FastAPI
@@ -10,6 +12,9 @@ data_path = os.path.join(current_dir, sample_data_path)
 with open(data_path) as json_books:
     books_data = json.load(json_books)
     book_list: list[Book] = Book.from_list(books_data)
+
+libraries: list[Library] = [Library("user1", "myLibrary"), Library(
+    "user1", "myOtherLibrary"), Library("user2", "generic")]
 
 
 mmr = FastAPI()
@@ -28,3 +33,41 @@ async def get_books():
 @mmr.get("/books/{isbn}")
 async def get_book(isbn: str):
     return list(filter(lambda book: book.to_dict()["ISBN"] == isbn, book_list))
+
+
+@mmr.get("/libraries/{user}")
+async def get_libraries(user: str):
+    libraries_from_user = filter(
+        lambda lib: lib.get_owner() == user, libraries)
+    return list(map(lambda lib: lib.to_dict(), libraries_from_user))
+
+
+@mmr.get("/libraries/{user}/{library}")
+async def get_library(user: str, library: str):
+    library = find_library(user, library)
+    return library
+
+
+@mmr.delete("/libraries/{user}/{library}")
+async def delete_library(user: str, library: str):
+    library = find_library(user, library)
+    if library:
+        libraries.remove(library)
+
+
+@mmr.put("/libraries/{user}/{library}/{new_name}")
+async def update_library_name(user: str, library: str, new_name: str):
+    library = find_library(user, library)
+    library.set_name(new_name)
+
+
+@mmr.post("/libraries/{user}/{library}")
+async def create_library(user: str, library: str):
+    libraries.append(Library(user, library))
+
+
+def find_library(owner: str, name: str) -> Library:
+    occurrencies = list(filter(
+        lambda lib: lib.get_owner() == owner and lib.get_name() == name, libraries))
+
+    return occurrencies[0] if len(occurrencies) else {}
