8ef29b0cd261446bc1317c3b49c70db022b04f3f
Ángel Píñar Rivas <angle@correo.ugr.es>
2022-11-06 17:42:25 +0100

    #57 Add missing property

--- mmr-backend/user_recommendation.py
+++ mmr-backend/user_recommendation.py
@@ -5,6 +5,7 @@ from typing import Tuple
 class UserRecommendation:
     def __init__(self, books: Tuple[str, str], user_comment: UserRecommendation.UserComment) -> None:
         self.__books = sorted(books)
+        self.__comments = [user_comment]
 
     class UserComment:
         def __init__(self, user: str, comment: str):
