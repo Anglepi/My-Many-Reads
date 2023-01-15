eaf338a7a1cf63e954c9b8bae89f8da30174520e
Ángel Píñar Rivas <angle@correo.ugr.es>
2023-01-15 17:51:58 +0100

    #77 privatize class methods

--- mmr-backend/data_manager.py
+++ mmr-backend/data_manager.py
@@ -2,9 +2,9 @@ class DataManager:
     def __init__(self, address: str) -> None:
         self._address = address
 
-    def connect(self) -> None:
+    def __connect(self) -> None:
         self.connection = open(self._address)
 
-    def disconnect(self) -> None:
+    def __disconnect(self) -> None:
         if self.connection:
             self.connection.close()
