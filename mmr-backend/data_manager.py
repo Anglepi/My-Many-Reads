822432e79ada41eeb51a545d9a849466f3557a50
Ángel Píñar Rivas <angle@correo.ugr.es>
2023-01-18 18:19:42 +0100

    #78 Refactor functionalities to look closer to real case

--- mmr-backend/data_manager.py
+++ mmr-backend/data_manager.py
@@ -1,5 +1,6 @@
 import json
 from book import Book
+from library import Library
 from typing import Optional
 
 
@@ -10,19 +11,28 @@ class DataManager:
     def __connect(self) -> None:
         self.connection = open(self._address)
 
+        # Set up fake data until real database is implemented
+        self.fake_books = json.load(self.connection)
+        self.fake_libraries = [Library("user1", "myLibrary", list()), Library(
+            "user1", "myOtherLibrary", list()), Library("user2", "generic", [Library.Entry(self.fake_books[0], 5, Library.ReadingStatus.COMPLETED)])]
+
     def __disconnect(self) -> None:
         if self.connection:
             self.connection.close()
+            del self.fake_books
+            del self.fake_libraries
 
     def get_books(self) -> list[Book]:
         self.__connect()
-        books_data = json.load(self.connection)
+        # To be replaced by an actual query
+        books = self.fake_books
         self.__disconnect()
-        return Book.from_list(books_data)
+        return Book.from_list(books)
 
     def get_book(self, isbn: str) -> Optional[Book]:
+        self.__connect()
         # To be replaced by an actual query
-        books: list[Book] = self.get_books()
         occurrencies = list(filter(lambda book: book.to_dict()[
-            "ISBN"] == isbn, books))
+            "ISBN"] == isbn, Book.from_list(self.fake_books)))
+        self.__disconnect()
         return occurrencies[0] if len(occurrencies) else None
