83086801aca05df1210952a1ba94ddfe6e21ec93
Ángel Píñar Rivas <angle@correo.ugr.es>
2023-05-01 14:46:49 +0200

    #96 Update tests reflecting usage of ID instead of ISBN

--- mmr-backend/test/test_user_recommendation.py
+++ mmr-backend/test/test_user_recommendation.py
@@ -5,11 +5,11 @@ from assertpy import assert_that
 def test_recommendation_equality():
     # Given - When
     recommendation1 = UserRecommendation(
-        ("ISBN1", "ISBN2"), UserRecommendation.UserComment("userA", "Both are cool"))
+        ("book_id1", "book_id2"), UserRecommendation.UserComment("userA", "Both are cool"))
     recommendation2 = UserRecommendation(
-        ("ISBN2", "ISBN1"), UserRecommendation.UserComment("userB", "Both are entertainning"))
+        ("book_id2", "book_id1"), UserRecommendation.UserComment("userB", "Both are entertainning"))
     recommendation3 = UserRecommendation(
-        ("ISBN5", "ISBN1"), UserRecommendation.UserComment("userC", "Both are scary"))
+        ("book_id5", "book_id1"), UserRecommendation.UserComment("userC", "Both are scary"))
 
     # Then
     assert_that(recommendation1).is_equal_to(recommendation2)
@@ -20,8 +20,8 @@ def test_recommendation_equality():
 def test_recommendation_dict():
     # Given
     recommendation1 = UserRecommendation(
-        ("ISBN1", "ISBN2"), UserRecommendation.UserComment("userA", "Both are cool"))
-    expected_dict = {"books": ["ISBN1", "ISBN2"], "comments": [
+        ("book_id1", "book_id2"), UserRecommendation.UserComment("userA", "Both are cool"))
+    expected_dict = {"books": ["book_id1", "book_id2"], "comments": [
         {"author": "userA", "comment": "Both are cool", "score": 0}]}
 
     # When
@@ -34,10 +34,10 @@ def test_recommendation_dict():
 def test_add_comment():
     # Given
     recommendation = UserRecommendation(
-        ("ISBN1", "ISBN2"), UserRecommendation.UserComment("userA", "Both are cool"))
+        ("book_id1", "book_id2"), UserRecommendation.UserComment("userA", "Both are cool"))
     new_comment = UserRecommendation.UserComment(
         "userB", "Both are entertainning")
-    expected_dict = {"books": ["ISBN1", "ISBN2"], "comments": [
+    expected_dict = {"books": ["book_id1", "book_id2"], "comments": [
         {"author": "userA", "comment": "Both are cool", "score": 0},
         {"author": "userB", "comment": "Both are entertainning", "score": 0}]}
 
@@ -51,10 +51,10 @@ def test_add_comment():
 def test_cannot_add_multiple_comments():
     # Given
     recommendation = UserRecommendation(
-        ("ISBN1", "ISBN2"), UserRecommendation.UserComment("userA", "Both are cool"))
+        ("book_id1", "book_id2"), UserRecommendation.UserComment("userA", "Both are cool"))
     new_comment = UserRecommendation.UserComment(
         "userA", "Both are entertainning")
-    expected_dict = {"books": ["ISBN1", "ISBN2"], "comments": [
+    expected_dict = {"books": ["book_id1", "book_id2"], "comments": [
         {"author": "userA", "comment": "Both are cool", "score": 0}]}
 
     # When
@@ -67,18 +67,18 @@ def test_cannot_add_multiple_comments():
 def test_has_book():
     # Given
     recommendation = UserRecommendation(
-        ("ISBN1", "ISBN2"), UserRecommendation.UserComment("userA", "Both are cool"))
+        ("book_id1", "book_id2"), UserRecommendation.UserComment("userA", "Both are cool"))
 
     # When - Then
-    assert_that(recommendation.has_book("ISBN1")).is_true()
-    assert_that(recommendation.has_book("ISBN4")).is_false()
+    assert_that(recommendation.has_book("book_id1")).is_true()
+    assert_that(recommendation.has_book("book_id4")).is_false()
 
 
 def test_get_author_comments():
     # Given
     user_comment = UserRecommendation.UserComment("userA", "Both are cool")
     recommendation = UserRecommendation(
-        ("ISBN1", "ISBN2"), user_comment)
+        ("book_id1", "book_id2"), user_comment)
 
     # When - Then
     assert_that(recommendation.get_author_comments("userA")[
@@ -89,8 +89,8 @@ def test_get_author_comments():
 def test_vote_comment():
     # Given
     recommendation = UserRecommendation(
-        ("ISBN1", "ISBN2"), UserRecommendation.UserComment("userA", "Both are cool"))
-    expected_dict = {"books": ["ISBN1", "ISBN2"], "comments": [
+        ("book_id1", "book_id2"), UserRecommendation.UserComment("userA", "Both are cool"))
+    expected_dict = {"books": ["book_id1", "book_id2"], "comments": [
         {"author": "userA", "comment": "Both are cool", "score": 1}]}
 
     # When
