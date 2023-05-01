19b15d879308e66de6c0cfa4b003df4ffe333b8f
Ángel Píñar Rivas <angle@correo.ugr.es>
2023-05-01 14:47:56 +0200

    #96 Update data manager to use book id instead of ISBN

--- mmr-backend/data_manager.py
+++ mmr-backend/data_manager.py
@@ -24,7 +24,7 @@ class DataManager:
 
     def get_books(self) -> list[Book]:
         self._cur.execute(
-            "SELECT b.isbn, b.title, b.synopsis, b.publisher, b.publishing_date, b.edition, " +
+            "SELECT b.id, b.isbn, b.title, b.synopsis, b.publisher, b.publishing_date, b.edition, " +
             "STRING_AGG(DISTINCT(a.name), %s) as authors, STRING_AGG(DISTINCT(g.genre), %s) " +
             "as genres FROM books b " +
             "left join authored ad on b.id = ad.book_id " +
@@ -41,17 +41,17 @@ class DataManager:
                 DataManager.AGGREGATED_VALUE_SEPARATOR)
         return Book.from_list(result)
 
-    def get_book(self, isbn: str) -> Optional[Book]:
+    def get_book(self, book_id: int) -> Optional[Book]:
         self._cur.execute(
-            "SELECT b.isbn, b.title, b.synopsis, b.publisher, b.publishing_date, b.edition, " +
+            "SELECT b.id, b.isbn, b.title, b.synopsis, b.publisher, b.publishing_date, b.edition, " +
             "STRING_AGG(DISTINCT(a.name), %s) as authors, STRING_AGG(DISTINCT(g.genre), %s) " +
             "as genres FROM books b " +
             "left join authored ad on b.id = ad.book_id " +
             "left join authors a on ad.author_id = a.id " +
             "left join book_genres bg on b.id = bg.book_id " +
             "left join genres g on g.id = bg.genre_id " +
-            "where b.isbn = %s " +
-            "group by b.id;", (DataManager.AGGREGATED_VALUE_SEPARATOR, DataManager.AGGREGATED_VALUE_SEPARATOR, isbn))
+            "where b.id = %s " +
+            "group by b.id;", (DataManager.AGGREGATED_VALUE_SEPARATOR, DataManager.AGGREGATED_VALUE_SEPARATOR, book_id))
         result = self._cur.fetchone()
         if not result:
             return None
@@ -67,7 +67,7 @@ class DataManager:
     def get_libraries_from_user(self, user: str) -> list[dict]:
         self._cur.execute(
             "SELECT l.id, l.owner, l.name, le.score, le.reading_status, " +
-            "b.isbn, b.title, b.synopsis, b.publisher, b.publishing_date, b.edition, " +
+            "b.id as book_id, b.isbn, b.title, b.synopsis, b.publisher, b.publishing_date, b.edition, " +
             "STRING_AGG(DISTINCT(a.name), %s) as authors, STRING_AGG(DISTINCT(g.genre), %s) as genres " +
             "from libraries l " +
             "left join library_entries le on l.id = le.library_id " +
@@ -85,10 +85,10 @@ class DataManager:
             if tuple["id"] not in libraries.keys():
                 libraries[tuple["id"]] = Library(
                     tuple["owner"], tuple["name"], [])
-            if tuple["isbn"]:
+            if tuple["book_id"]:
                 tuple["authors"] = tuple["authors"].split(";")
                 tuple["genres"] = tuple["genres"].split(";")
-                libraries[tuple["id"]].add_entry(Library.Entry(Book(tuple["isbn"], tuple["title"], tuple["synopsis"], tuple["authors"],
+                libraries[tuple["id"]].add_entry(Library.Entry(Book(tuple["book_id"], tuple["isbn"], tuple["title"], tuple["synopsis"], tuple["authors"],
                                                  tuple["genres"], tuple["publisher"], tuple["publishing_date"], tuple["edition"]), tuple["score"], tuple["reading_status"]))
 
         return list(map(lambda lib: lib.to_dict(), libraries.values())) if libraries else []
@@ -96,7 +96,7 @@ class DataManager:
     def get_library(self, user: str, library_name: str) -> Optional[Library]:
         self._cur.execute(
             "SELECT l.id, l.owner, l.name, le.score, le.reading_status, " +
-            "b.isbn, b.title, b.synopsis, b.publisher, b.publishing_date, b.edition, " +
+            "b.id as book_id, b.isbn, b.title, b.synopsis, b.publisher, b.publishing_date, b.edition, " +
             "STRING_AGG(DISTINCT(a.name), %s) as authors, STRING_AGG(DISTINCT(g.genre), %s) as genres " +
             "from libraries l " +
             "left join library_entries le on l.id = le.library_id " +
@@ -114,10 +114,10 @@ class DataManager:
 
         library: Library = Library(result[0]["owner"], result[0]["name"], [])
         for tuple in result:
-            if tuple["isbn"]:
+            if tuple["book_id"]:
                 tuple["authors"] = tuple["authors"].split(";")
                 tuple["genres"] = tuple["genres"].split(";")
-                library.add_entry(Library.Entry(Book(tuple["isbn"], tuple["title"], tuple["synopsis"], tuple["authors"],
+                library.add_entry(Library.Entry(Book(tuple["book_id"], tuple["isbn"], tuple["title"], tuple["synopsis"], tuple["authors"],
                                                      tuple["genres"], tuple["publisher"], tuple["publishing_date"], tuple["edition"]), tuple["score"], tuple["reading_status"]))
 
         return library
@@ -144,17 +144,17 @@ class DataManager:
 
         return True
 
-    def remove_library_entry(self, user: str, library_name: str, isbn: str) -> None:
+    def remove_library_entry(self, user: str, library_name: str, book_id: int) -> None:
         self._cur.execute("delete from library_entries le using libraries l, books b " +
                           "where le.library_id = l.id and b.id = le.book_id and " +
-                          "b.isbn = %s and l.owner = %s and l.name = %s", (isbn, user, library_name))
+                          "b.id = %s and l.owner = %s and l.name = %s", (book_id, user, library_name))
         self._connection.commit()
 
-    def add_library_entry(self, user: str, library_name: str, isbn: str) -> bool:
+    def add_library_entry(self, user: str, library_name: str, book_id: int) -> bool:
         try:
             self._cur.execute("insert into library_entries(library_id, book_id) " +
                               "select l.id, b.id from libraries l, books b " +
-                              "where l.owner = %s AND l.name = %s AND b.isbn = %s", (user, library_name, isbn))
+                              "where l.owner = %s AND l.name = %s AND b.id = %s", (user, library_name, book_id))
         except psycopg2.IntegrityError:
             self._connection.rollback()
             return False
@@ -162,30 +162,30 @@ class DataManager:
             self._connection.commit()
         return True
 
-    def update_library_entry(self, user: str, library_name: str, isbn: str, score: int, status: Library.ReadingStatus) -> None:
+    def update_library_entry(self, user: str, library_name: str, book_id: int, score: int, status: Library.ReadingStatus) -> None:
         self._cur.execute("update library_entries le set book_id = b.id, score=%s, reading_status=%s " +
                           "from books b, libraries l " +
-                          "where l.owner = %s AND l.name = %s AND b.isbn = %s " +
-                          "AND le.library_id = l.id AND le.book_id = b.id", (score, status.value, user, library_name, isbn))
+                          "where l.owner = %s AND l.name = %s AND b.id = %s " +
+                          "AND le.library_id = l.id AND le.book_id = b.id", (score, status.value, user, library_name, book_id))
         self._connection.commit()
 
     # RECOMMENDATIONS
 
-    def get_recommendations_for_book(self, isbn: str) -> list[dict]:
-        self._cur.execute("select ur.id, b1.isbn as isbn1, b2.isbn as isbn2, urc.author, urc.comment, urc.score " +
+    def get_recommendations_for_book(self, book_id: int) -> list[dict]:
+        self._cur.execute("select ur.id, b1.id as book_id1, b2.id as book_id2, urc.author, urc.comment, urc.score " +
                           "from user_recommendations ur " +
                           "left join books b1 on ur.book1_id = b1.id " +
                           "left join books b2 on ur.book2_id = b2.id " +
                           "left join user_recommendation_comments urc on ur.id = urc.recommendation_id " +
-                          "where b1.isbn = %s or b2.isbn = %s " +
-                          "order by urc.score desc, urc.author asc, urc.comment asc", (isbn, isbn))
+                          "where b1.id = %s or b2.id = %s " +
+                          "order by urc.score desc, urc.author asc, urc.comment asc", (book_id, book_id))
 
         result = self._cur.fetchall()
         recommendations: dict[int, UserRecommendation] = {}
         for tuple in result:
             if tuple["id"] not in recommendations.keys():
                 recommendations[tuple["id"]] = UserRecommendation(
-                    (tuple["isbn1"], tuple["isbn2"]),
+                    (tuple["book_id1"], tuple["book_id2"]),
                     UserRecommendation.UserComment(
                         tuple["author"], tuple["comment"], tuple["score"])
                 )
@@ -195,28 +195,28 @@ class DataManager:
 
         return list(map(lambda recommendation: recommendation.to_dict(), recommendations.values()))
 
-    def __getOrCreateRecommendationsPair(self, isbn1: str, isbn2: str) -> Optional[int]:
+    def __getOrCreateRecommendationsPair(self, book_id1: int, book_id2: int) -> Optional[int]:
         self._cur.execute("select ur.id from user_recommendations ur " +
                           "left join books b1 on b1.id = ur.book1_id " +
                           "left join books b2 on b2.id = ur.book2_id " +
                           "where " +
-                          "(b1.isbn = %s and b2.isbn = %s) " +
+                          "(b1.id = %s and b2.id = %s) " +
                           "or  " +
-                          "(b2.isbn = %s and b1.isbn = %s)", (isbn1, isbn2, isbn1, isbn2))
+                          "(b2.id = %s and b1.id = %s)", (book_id1, book_id2, book_id1, book_id2))
         result = self._cur.fetchone()
 
         if not result:
             self._cur.execute("insert into user_recommendations (book1_id, book2_id) " +
                               "select b1.id as book1_id, b2.id as book2_id from books b1 " +
-                              "join books b2 on b1.isbn = %s and b2.isbn=%s " +
-                              "returning id", (isbn1, isbn2))
+                              "join books b2 on b1.id = %s and b2.id=%s " +
+                              "returning id", (book_id1, book_id2))
             result = self._cur.fetchone()
 
         return result["id"] if result else None
 
-    def create_user_recommendation(self, isbn1: str, isbn2: str, user: str, comment: str) -> int:
+    def create_user_recommendation(self, book_id1: int, book_id2: int, user: str, comment: str) -> int:
         recommendationPairId: Optional[int] = self.__getOrCreateRecommendationsPair(
-            isbn1, isbn2)
+            book_id1, book_id2)
 
         if not recommendationPairId:
             return 404
@@ -234,15 +234,15 @@ class DataManager:
         result = self._cur.fetchone()
         return 201 if result else 404
 
-    def vote_user_recommendation(self, isbn1: str, isbn2: str, user: str) -> bool:
+    def vote_user_recommendation(self, book_id1: int, book_id2: int, user: str) -> bool:
         self._cur.execute("update user_recommendation_comments urc " +
                           "set score = score + 1 " +
                           "from books b1 " +
-                          "join books b2 on b1.isbn = %s and b2.isbn= %s " +
+                          "join books b2 on b1.id = %s and b2.id= %s " +
                           "join user_recommendations ur " +
                           "on (ur.book1_id = b1.id and ur.book2_id = b2.id) or (ur.book1_id = b2.id and ur.book2_id = b1.id) " +
                           "where ur.id = urc.recommendation_id and urc.author = %s " +
-                          "returning urc.id", (isbn1, isbn2, user))
+                          "returning urc.id", (book_id1, book_id2, user))
         self._connection.commit()
         result = self._cur.fetchone()
         return True if result else False
